3a158f9e3044f678c8d24da850f142f9d7a7872a
evanmiller29 <evanmiller29@gmail.com>
2017-12-30 21:11:04 +1300

    Moving away from keras tutorial to a more easily understandable numpy basis (from kaggle kernels)

--- first_cnn.py
+++ first_cnn.py
@@ -13,7 +13,6 @@ from os.path import join
 from tqdm import tqdm
 import cv2
 
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -89,125 +88,8 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-df_train['file'] = df_train['id']  + '.jpg'
-
-df_test = pd.read_csv(join(data_dir, 'sample_submission.csv'))
-
-targets_series = pd.Series(df_train['breed'])
-NUM_CLASSES = targets_series.unique().shape[0]
-
-doggo_types = set(targets_series)
-
-if create_dirs:
-
-    fn.create_train_valid_dirs(doggo_types, data_dir)
-
-os.chdir(data_dir)
-
-img_width, img_height = 500, 375
-
-train_data_dir = 'train'
-validation_data_dir = 'validation'
-nb_train_samples = 7143
-nb_validation_samples = 3079
-epochs = 50
-batch_size = 16
-
-if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
-else:
-    input_shape = (img_width, img_height, 3)
-
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-model.add(Dense(NUM_CLASSES, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
-# this is the augmentation configuration we will use for testing:
-# only rescaling
-
-valid_path = join(data_dir, 'validation')
-train_path = join(data_dir, 'train')
-
-test_datagen = ImageDataGenerator(rescale=1. / 255)
-
-train_generator = train_datagen.flow_from_directory(
-    train_path,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    classes= list(doggo_types),
-    class_mode='categorical')
-
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    classes= list(doggo_types),
-    class_mode='categorical')
-
-model.fit_generator(
-    train_generator,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
+model.fit(X_train, Y_train, epochs=50,
+          validation_data=(X_valid, Y_valid), verbose=1)
 
 model.save('first_cnn.h5')
 
